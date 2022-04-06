a96db10cb01a2ca84f318cbbe2eb6cadd960bc19
Emma Perlman <emmajperlman@gmail.com>
2022-04-06 13:43:08 -0700

    commit tree 1261e8e2e70a30dd440171ea321ec48d679a8159

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkList=links.map((ref)=>{
+    return <a href={'#'+ref} key={ref}>{ref}</a>;
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{linkList}</nav>;
 }
 
 export default NavBar;
